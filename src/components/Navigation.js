5f19cf0f42b1303a26b8698362869fb8d6d5d0bb
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2023-10-24 07:00:32 +0200

    navbar and logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
+import logo from "../img/logo3.png";
 
 function Navigation() {
   return (
     <nav className="navbar">
+      <img src={logo} className="logo-image" alt="logo" />
       <ul>
         <li id="navbar-home">
           <Link to="/" className="navbar-menu">
